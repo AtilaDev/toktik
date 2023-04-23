4c17940a5f4a6bc10874e758b72bd0b5ac27846b
Leandro <atiladevelop@gmail.com>
2023-04-22 21:34:39 -0300

    Repository and Datasource implemented

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -12,8 +14,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository = VideoPostsRepositoryImpl(
+      videoPostsDatasource: LocalVideoDatasource(),
+    );
+
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => DiscoverProvider())],
+      providers: [
+        ChangeNotifierProvider(
+          lazy: false,
+          create: (_) =>
+              DiscoverProvider(videoPostRepository: videoPostRepository)
+                ..loadNextPage(),
+        )
+      ],
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'TokTik',
--- lib/shared/data/local_video_posts.dart
+++ lib/shared/data/local_video_posts.dart
@@ -1,49 +1,57 @@
 List<Map<String, dynamic>> videoPosts = [
   {
     'name': 'Subiendo escaleras automáticas',
-    'videoUrl': 'assets/videos/1.mp4',
+    'videoUrl':
+        'https://player.vimeo.com/external/476838909.sd.mp4?s=33e4e8ec8dcd99aefd4eda56737c498ac69c8c1f&profile_id=165&oauth2_token_id=57447761',
     'likes': 23230,
     'views': 1523,
   },
   {
     'name': 'Planta apreciada por peatones',
-    'videoUrl': 'assets/videos/2.mp4',
+    'videoUrl':
+        'https://player.vimeo.com/external/402571180.sd.mp4?s=ce6aef8e9259c29cde79e3db0caf87c0ac213d86&profile_id=165&oauth2_token_id=57447761',
     'likes': 24230,
     'views': 1343,
   },
   {
     'name': 'Que borroso veo todo!',
-    'videoUrl': 'assets/videos/3.mp4',
+    'videoUrl':
+        'https://player.vimeo.com/external/435882538.sd.mp4?s=02d860843242a89b819cde85b2849aeb38065e09&profile_id=165&oauth2_token_id=57447761',
     'likes': 21564320,
     'views': 123563,
   },
   {
     'name': '¿Esto es trigo? que interesante',
-    'videoUrl': 'assets/videos/4.mp4',
+    'videoUrl':
+        'https://player.vimeo.com/external/479606760.sd.mp4?s=ce79317c33c0ce25b7890b0e52f2852a44704d31&profile_id=165&oauth2_token_id=57447761',
     'likes': 320,
     'views': 2300,
   },
   {
     'name': 'El COVID no me afecta',
-    'videoUrl': 'assets/videos/5.mp4',
+    'videoUrl':
+        'https://player.vimeo.com/external/398518760.hd.mp4?s=d27e3d698f8dc07ece5fc0e1eb7b8c2404353dac&profile_id=174&oauth2_token_id=57447761',
     'likes': 3230,
     'views': 31030,
   },
   {
     'name': 'No quiero ir a trabajar hoy señor Stark',
-    'videoUrl': 'assets/videos/6.mp4',
+    'videoUrl':
+        'https://player.vimeo.com/external/403270649.hd.mp4?s=b85f22f7ffd137bd50331c1881c0d82631f1f0e7&profile_id=174&oauth2_token_id=57447761',
     'likes': 10,
     'views': 330,
   },
   {
     'name': 'Limpiar nunca fue tan divertido',
-    'videoUrl': 'assets/videos/7.mp4',
+    'videoUrl':
+        'https://player.vimeo.com/external/403652508.hd.mp4?s=619899d6bfc52d8832cad04c0c0be57d45bb6c02&profile_id=174&oauth2_token_id=57447761',
     'likes': 1320,
     'views': 33032,
   },
   {
     'name': '¿Ya llegamos a la India?... umm si',
-    'videoUrl': 'assets/videos/8.mp4',
+    'videoUrl':
+        'https://player.vimeo.com/external/420234383.sd.mp4?s=bbe7a79800aaced5acde961d42af3838a17d4741&profile_id=165&oauth2_token_id=57447761',
     'likes': 342,
     'views': 3332,
   },
